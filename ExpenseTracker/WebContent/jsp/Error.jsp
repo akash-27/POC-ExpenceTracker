ad33250d37234d323cb8e0a8510a28758b2f1e6d
kiran <kiran.yadav@globalogic.com>
2015-09-07 15:04:36 +0530

    "Logout and Edit profile changes"

---
+++ ExpenseTracker/WebContent/jsp/Error.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<p><h1>Some Error</h1>
+	<p><h2>User Profile Updation Failed</h2>
+	<a href="welcome.jsp">try again </a>
+</body>
+</html>
---
+++ ExpenseTracker/src/com/gl/expensetracker/usrprof/VerifyOldPass.java
@@ -0,0 +1,57 @@
+package com.gl.expensetracker.usrprof;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.gl.expencetracker.ExpenseUser;
+
+/**
+ * Servlet implementation class VerifyOldPass
+ */
+@WebServlet("/jsp/VerifyOldPass")
+public class VerifyOldPass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public VerifyOldPass() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String oldpass = request.getParameter("oldpass");
+		HttpSession session = request.getSession(true);
+		ExpenseUser curruser = new ExpenseUser();
+		curruser = (ExpenseUser) session.getAttribute("user");
+		String Msg = null;
+		response.setContentType("text/plain");
+		if(!(oldpass.equals(curruser.getPassWord()))){
+			 Msg = "FAILURE";
+		}
+		else{
+			 Msg = "SUCCESS";
+		}
+		
+		response.getWriter().write(Msg);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
