b78d3fec9806d2915911be3a99e0ea27e1e89022
akash kumar <akash.kumar@globallogic.com>
2015-09-28 10:34:52 +0530

    modified mailer api content

--- ExpenseTracker/src/com/gl/mailapi/MailAPI.java
+++ ExpenseTracker/src/com/gl/mailapi/MailAPI.java
@@ -4,9 +4,10 @@ import com.gl.scheduler.SendMail;
 
 public class MailAPI {
 	
-	public static SendMail mailer ;
+	
 	public  void VerificationMail(String toId)
 	{
+		SendMail mailer = new SendMail();
 		String sub = "Account Verification";
 		String msg = "Hi,\n You have been registred at Expence Tracker. Please verify your account at below link."
 				+ "\nThis is an automated mail please do not reply to this."
@@ -16,28 +17,29 @@ public class MailAPI {
 	}
 	public static void RegisterMail(String toId)
 	{
-		SendMail mailer1 = new SendMail();
+		SendMail mailer = new SendMail();
 		String sub = "Account Registration";
 		String msg = "Hi";
 		msg += "<br>"+" You have been successfully registered at Expense Tracker.";
 		msg += "<br>"+"This is an automated mail please do not reply to this.";
 		msg += "<br>"+"Thanks ";
 		
-		mailer1.sendMessageToMail(toId, sub, msg);		
+		mailer.sendMessageToMail(toId, sub, msg);		
 	}
 	public static void PasswordChange(String toId)
 	{
-		SendMail mailer1 = new SendMail();
+		SendMail mailer = new SendMail();
 		String sub = "Password change notification";
 		String msg = "Hi,"+"<br>"+"Your password has been successfully changed. If you have not"
 				+ " changed your password, please visit our site for further details."
 				+ "<br>"+"This is an automated mail please do not reply to this."
 				+ "<br>"+"Thanks ";
 		
-		mailer1.sendMessageToMail(toId, sub, msg);		
+		mailer.sendMessageToMail(toId, sub, msg);		
 	}
 	public static void AddMemberNotify(String toId)
 	{
+		SendMail mailer = new SendMail();
 		String sub = "AddMember Notification";
 		String msg = "Hi,"+"<br>"+"Someone has added you as a member in his group. Log in to our"
 				+ " website for more details."
@@ -48,6 +50,7 @@ public class MailAPI {
 	}
 	public static void CreateMemberNotify(String toId)
 	{
+		SendMail mailer = new SendMail();
 		String sub = "Group Created";
 		String msg = "Hi, \n You have successfully created a group in your Expense Tracker account"
 				+ "\nThis is an automated mail please do not reply to this."
